60856c03cefa23566aa82b31a6dda7902a9a7e57
BonugliaGabriele <82998947+Bonouou@users.noreply.github.com>
2022-05-05 10:02:44 +0200

    Inizio Carica appunto

---
+++ CodiceBonuglia/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace Prova2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                var file = await FilePicker.PickAsync();
+                if (file == null)
+                    return;
+
+                LabelInfo.Text = file.FileName;
+
+            }
+            catch (Exception) { 
+            
+            }
+           
+        }
+    }
+}
